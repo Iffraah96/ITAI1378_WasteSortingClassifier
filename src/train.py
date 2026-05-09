b91cdf3f33be7c209fc55a8e52ec7218a37c3422
Iffraah Rehman <iffraah96@gmail.com>
2026-05-09 06:56:59 -0500

    Create train.py

---
+++ src/train.py
@@ -0,0 +1,27 @@
+IMG_SIZE = (224, 224)
+BATCH_SIZE = 32
+SEED = 42
+
+train_ds = tf.keras.utils.image_dataset_from_directory(
+    DATASET_PATH,
+    validation_split=0.2,
+    subset="training",
+    seed=SEED,
+    image_size=IMG_SIZE,
+    batch_size=BATCH_SIZE
+)
+
+val_ds = tf.keras.utils.image_dataset_from_directory(
+    DATASET_PATH,
+    validation_split=0.2,
+    subset="validation",
+    seed=SEED,
+    image_size=IMG_SIZE,
+    batch_size=BATCH_SIZE
+)
+
+class_names = train_ds.class_names
+num_classes = len(class_names)
+
+print("Class names:", class_names)
+print("Number of classes:", num_classes)
